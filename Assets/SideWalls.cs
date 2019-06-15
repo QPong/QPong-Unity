79c735b4e92edc3cb8254eecc71d67bdffa16307
Huang Junye <h.jun.ye@gmail.com>
2019-06-15 16:49:28 +0800

    Add left and right wall trigger for scoring

---
+++ Assets/SideWalls.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideWalls : MonoBehaviour
+{
+    
+    void OnTriggerEnter2D(Collider2D hitInfo){
+        if (hitInfo.name == "Ball"){
+            string wallName = transform.name;
+            GameManager.Score(wallName);
+            hitInfo.gameObject.SendMessage("RestartGame", 1.0f, SendMessageOptions.RequireReceiver);
+        }
+    }
+
+}
