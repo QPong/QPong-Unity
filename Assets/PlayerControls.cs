191b21b76195ff46ebdb3db53f95d6bb54686132
Huang Junye <h.jun.ye@gmail.com>
2019-06-15 14:05:35 +0800

    Implement player control

---
+++ Assets/PlayerControls.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public KeyCode moveUp = KeyCode.W;
+    public KeyCode moveDown = KeyCode.S;
+    public float speed = 10.0f;
+    public float boundY = 2.25f;
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var vel = rb2d.velocity;
+        if (Input.GetKey(moveUp)) {
+            vel.y = speed;
+        }
+        else if (Input.GetKey(moveDown)) {
+            vel.y = -speed;
+        }
+        else {
+            vel.y = 0;
+        }
+        rb2d.velocity = vel;
+
+        var pos = transform.position;
+        if (pos.y > boundY) {
+            pos.y = boundY;
+        }
+        else if (pos.y < -boundY) {
+            pos.y = -boundY;
+        }
+        transform.position = pos;
+    }
+}
