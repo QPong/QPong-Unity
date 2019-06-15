180e8d4fb28573299d1f64bfc65201429a206107
Huang Junye <h.jun.ye@gmail.com>
2019-06-15 14:32:36 +0800

    Add ball

---
+++ Assets/BallControl.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+
+    void GoBall(){
+        float rand = Random.Range(0, 2);
+        if (rand < 1) {
+            rb2d.AddForce(new Vector2(20, -15));
+        } else {
+            rb2d.AddForce(new Vector2(-20, -15));
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        Invoke("GoBall", 2); // wait for 2 seconds to give players time to get ready
+    }
+
+    void ResetBall(){
+        rb2d.velocity = Vector2.zero;
+        transform.position = Vector2.zero;
+    }
+
+    void RestartGame(){
+        ResetBall();
+        Invoke("GoBall", 1);
+    }
+
+    void OnCollisionEnter2D(Collision2D coll){
+        if (coll.collider.CompareTag("Player")){
+            Vector2 vel;
+            vel.x = rb2d.velocity.x;
+            vel.y = (rb2d.velocity.y / 2.0f) + (coll.collider.attachedRigidbody.velocity.y / 3.0f);
+            rb2d.velocity = vel;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
