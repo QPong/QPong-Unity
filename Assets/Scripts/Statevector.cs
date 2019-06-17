e44341e699603040ea892209221565ec46e96f92
Huang Junye <h.jun.ye@gmail.com>
2019-06-17 22:53:42 +0800

    Add quantum circuit background and gates

---
+++ Assets/Scripts/Statevector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Statevector : MonoBehaviour
+{
+    public GUISkin layout;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnGUI(){
+        GUI.skin = layout;
+        GUI.Label(new Rect(100, 20, 100, 100), "|000>");
+        // GUI.Label(new Rect(100, 20, 100, 100), "|001>");
+
+        // state0.transform.rotation =Quaternion.Eulers(0,0,90.0f);
+
+    }
+}
