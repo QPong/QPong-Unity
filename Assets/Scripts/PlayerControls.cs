9b64830663126c5dc2522877d4c11e2769b816de
Huang Junye <h.jun.ye@gmail.com>
2019-06-19 21:21:05 +0800

    Implement cursor movement
    Modify PlayerControl.cs to control circuit grid cursor. Remove PlayerControl scripts from paddles

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -6,38 +6,46 @@ public class PlayerControls : MonoBehaviour
 {
     public KeyCode moveUp = KeyCode.W;
     public KeyCode moveDown = KeyCode.S;
-    public float speed = 10.0f;
-    public float boundY = 2.25f;
-    private Rigidbody2D rb2d;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb2d = GetComponent<Rigidbody2D>();   
-    }
+    public KeyCode moveLeft = KeyCode.A;
+    public KeyCode moveRight = KeyCode.D;
+    public float stepX = 5.0f;
+    public float stepY = 5.0f;
+    public float boundX = 5.0f;
+    public float boundY = 35.0f;
+    public float centerX = 56.0f;
 
     // Update is called once per frame
     void Update()
     {
-        var vel = rb2d.velocity;
-        if (Input.GetKey(moveUp)) {
-            vel.y = speed;
-        }
-        else if (Input.GetKey(moveDown)) {
-            vel.y = -speed;
-        }
-        else {
-            vel.y = 0;
-        }
-        rb2d.velocity = vel;
-
         var pos = transform.position;
+
+        if (Input.GetKeyDown(moveUp)) {
+            pos.y += stepY;
+        }
+        else if (Input.GetKeyDown(moveDown)) {
+            pos.y += -stepY;
+        }
+        else if (Input.GetKeyDown(moveLeft)) {
+            pos.x += -stepX;
+        }
+        else if (Input.GetKeyDown(moveRight)) {
+            pos.x += stepX;
+        }
+
         if (pos.y > boundY) {
             pos.y = boundY;
         }
         else if (pos.y < -boundY) {
             pos.y = -boundY;
         }
+
+        if (pos.x > centerX + boundX) {
+            pos.x = centerX + boundX;
+        }
+        else if (pos.x < centerX - boundX) {
+            pos.x = centerX - boundX;
+        }
+
         transform.position = pos;
     }
 }
