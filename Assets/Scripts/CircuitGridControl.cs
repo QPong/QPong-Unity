cd920746855d323fda6f91ad36527b2b0f69344d
Huang Junye <h.jun.ye@gmail.com>
2019-06-24 22:32:21 +0800

    Implement gate selection via GameObject name
    Gate array is name as gate[column][row]

--- Assets/Scripts/CircuitGridControl.cs
+++ Assets/Scripts/CircuitGridControl.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class CircuitGridControl : MonoBehaviour
 {
     public GameObject emptyGate;
-    public int columnMax = 3;
-    public int rowMax = 4;
+    public int columnMax = 14;
+    public int rowMax = 3;
     public int columnHeight = 5;
     public int rowHeight = 5;
-    public GameObject[] gateArray;  //1D array of gate
+    public float xOffset = -51f;
+    public float yOffset = -30f;
+    private GameObject[] gateArray;  //1D array of gate
     public GameObject selectedGate;
-    public int selectedGateIndex;
+    public int selectedColNum;
+    public int selectedRowNum;
 
     public KeyCode moveUp = KeyCode.W;
     public KeyCode moveDown = KeyCode.S;
@@ -28,19 +31,42 @@ public class CircuitGridControl : MonoBehaviour
             for (int j = 0; j < rowMax; j++)
             {
                 int index = i + j * rowMax;
-                gateArray[index] = (GameObject)Instantiate(emptyGate, new Vector2(i * columnHeight, j * rowHeight), Quaternion.identity);
+                gateArray[index] = (GameObject)Instantiate(emptyGate, new Vector2(xOffset + j * rowHeight, yOffset + i * columnHeight), 
+                Quaternion.Euler(new Vector3(0f, 0f, 90f)));  // initiate gates with 90 degrees rotation
+                gateArray[index].name = "gate["+i+"]["+j+"]";
             }
         }
-        selectedGate = gateArray[1];
+        selectedGate = GameObject.Find("gate[0][0]");;
     }
 
     // Update is called once per frame
     void Update()
     {
+        selectedColNum = (int) System.Char.GetNumericValue(selectedGate.name[5]);
+        selectedRowNum = (int) System.Char.GetNumericValue(selectedGate.name[8]);
+
         if (Input.GetKeyDown(moveDown)) {
-            selectedGateIndex = System.Array.IndexOf(gateArray, selectedGate);
-            selectedGate = gateArray[selectedGateIndex + rowMax];
-            Debug.Log("Selected gate index is"+selectedGateIndex);
+            selectedRowNum ++;
+        } else if (Input.GetKeyDown(moveUp)) {
+            selectedRowNum --;
+        } else if (Input.GetKeyDown(moveRight)) {
+            selectedColNum ++;
+        } else if (Input.GetKeyDown(moveLeft)) {
+            selectedColNum --;
         }
+
+        if (selectedColNum >= columnMax) {
+            selectedColNum = columnMax - 1;
+        } else if (selectedColNum < 0) {
+            selectedColNum = 0;
+        }
+
+        if (selectedRowNum >= rowMax) {
+            selectedRowNum = rowMax - 1;
+        } else if (selectedRowNum < 0) {
+            selectedRowNum = 0;
+        }
+
+        selectedGate = GameObject.Find("gate["+selectedColNum+"]["+selectedRowNum+"]");
     }
 }
