87d0245e3ff188f35bea4633dfeb50d21dcdc8d1
Huang Junye <h.jun.ye@gmail.com>
2019-06-25 09:09:25 +0800

    Add X,Y,Z,H gates by changing spirtes

--- Assets/Scripts/CircuitGridControl.cs
+++ Assets/Scripts/CircuitGridControl.cs
@@ -5,22 +5,31 @@ using UnityEngine;
 public class CircuitGridControl : MonoBehaviour
 {
     public GameObject emptyGate;
-    public int columnMax = 14;
+    public int columnMax = 15;
     public int rowMax = 3;
     public int columnHeight = 5;
     public int rowHeight = 5;
     public float xOffset = -51f;
-    public float yOffset = -30f;
+    public float yOffset = -35f;
     private GameObject[] gateArray;  //1D array of gate
     public GameObject selectedGate;
     public int selectedColNum;
     public int selectedRowNum;
+    public GameObject cursor;
+
+    public Sprite XGateSprite;
+    public Sprite YGateSprite;
+    public Sprite ZGateSprite;
+    public Sprite HGateSprite;
 
     public KeyCode moveUp = KeyCode.W;
     public KeyCode moveDown = KeyCode.S;
     public KeyCode moveLeft = KeyCode.A;
     public KeyCode moveRight = KeyCode.D;
     public KeyCode addXGate = KeyCode.X;
+    public KeyCode addYGate = KeyCode.Y;
+    public KeyCode addZGate = KeyCode.Z;
+    public KeyCode addHGate = KeyCode.H;
 
     // Start is called before the first frame update
     void Start()
@@ -32,11 +41,12 @@ public class CircuitGridControl : MonoBehaviour
             {
                 int index = i + j * rowMax;
                 gateArray[index] = (GameObject)Instantiate(emptyGate, new Vector2(xOffset + j * rowHeight, yOffset + i * columnHeight), 
-                Quaternion.Euler(new Vector3(0f, 0f, 90f)));  // initiate gates with 90 degrees rotation
+                    Quaternion.Euler(new Vector3(0f, 0f, 90f)));  // initiate gates with 90 degrees rotation
                 gateArray[index].name = "gate["+i+"]["+j+"]";
             }
         }
-        selectedGate = GameObject.Find("gate[0][0]");;
+        selectedGate = GameObject.Find("gate[0][0]");
+        cursor = GameObject.Find("Cursor");
     }
 
     // Update is called once per frame
@@ -67,6 +77,17 @@ public class CircuitGridControl : MonoBehaviour
             selectedRowNum = 0;
         }
 
+        if (Input.GetKeyDown(addXGate)) {
+            selectedGate.GetComponent<SpriteRenderer>().sprite = XGateSprite;
+        } else if (Input.GetKeyDown(addYGate)) {
+            selectedGate.GetComponent<SpriteRenderer>().sprite = YGateSprite;
+        } else if (Input.GetKeyDown(addZGate)) {
+            selectedGate.GetComponent<SpriteRenderer>().sprite = ZGateSprite;
+        } else if (Input.GetKeyDown(addHGate)) {
+            selectedGate.GetComponent<SpriteRenderer>().sprite = HGateSprite;
+        }
+
         selectedGate = GameObject.Find("gate["+selectedColNum+"]["+selectedRowNum+"]");
+        cursor.transform.position = selectedGate.transform.position;
     }
 }
