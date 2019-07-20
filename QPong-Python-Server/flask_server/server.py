a20e99c2fbbc002c00fbce64263967d967401cc4
Huang Junye <h.jun.ye@gmail.com>
2019-07-20 11:31:06 +0800

    Remove backend_config() and qasm() api

--- QPong-Python-Server/flask_server/server.py
+++ QPong-Python-Server/flask_server/server.py
@@ -20,38 +20,6 @@ def welcome():
     return "Hi Qiskiter!"
 
 
-@app.route('/api/backend/configuration')
-def backend_config():
-    config = backend_configuration('qasm_simulator')
-    return jsonify({"result": config})
-
-
-@app.route('/api/run/qasm', methods=['POST'])
-def qasm():
-    qasm = request.form.get('qasm')
-    api_token = None
-    if request.form.get('api_token'):
-        api_token = request.form.get('api_token')
-
-    shots = 1024
-    if request.form.get('shots'):
-        shots = int(request.form.get('shots'))
-
-    memory = False
-    if request.form.get('memory'):
-        memory = request.form.get('memory').lower() in ['true', '1']
-
-    print("--------------")
-    print(qasm)
-    print(request.get_data())
-    print(request.form)
-    backend = 'qasm_simulator'
-    output = run_qasm(qasm, backend, api_token=api_token, shots=shots, memory=memory)
-
-    ret = {"result": output}
-    return jsonify(ret)
-
-
 @app.route('/api/run/get_statevector', methods=['POST'])
 def get_statevector():
     gate_array_string = request.form.get('gate_array')
