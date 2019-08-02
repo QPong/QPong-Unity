f6fa843a65cb20beaa3518eb4521bac0f4d81f6d
Ivan Duran <iduran@us.ibm.com>
2019-08-02 13:14:21 +0200

    Add basic objects to control the game

---
+++ QPong-Unity-Version/Assets/Scripts/Definitions.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+// Game enums
+public enum Scenes { MainScreen = 0, Game, Levels, HowToPlay, About};
+
+public class Definitions : MonoBehaviour {
+    // Static singleton Instance
+    private static Definitions instance;
+
+    // Static singleton instance
+    public static Definitions Instance
+    {
+        get { return instance ?? (instance = new GameObject("Definitions").AddComponent<Definitions>()); }
+    }
+}
---
+++ QPong-Unity-Version/Assets/Scripts/GameController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    // Static singleton Instance
+    private static GameController instance;
+
+    // Static singleton instance
+    public static GameController Instance
+    {
+        get { return instance ?? (instance = new GameObject("GameController").AddComponent<GameController>()); }
+    }
+
+    // Global vars
+    public Player player;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    private void OnDestroy()
+    {
+        SceneManager.activeSceneChanged -= LevelWasLoaded;
+    }
+
+    // Use this for initialization
+    private void Start () {
+
+	}
+
+    public void OpenScene (Scenes scene) {
+        SceneManager.LoadScene(scene.ToString());
+    }
+
+    public void LevelWasLoaded(Scene previousScene, Scene newScene)
+    {
+        // Check if exit from game
+        if (previousScene.name == Scenes.Game.ToString()) {
+
+        }
+    }
+}
