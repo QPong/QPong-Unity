1fffc2c28bdc4075e31dc90ea541d86b549a86a0
Ivan Duran <iduran@us.ibm.com>
2019-08-19 21:12:13 +0200

    Fill ranking list

---
+++ QPong-Unity-Version/Assets/Scripts/Ranking/RowHUD.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class RowHUD : MonoBehaviour
+{
+    public Text position;
+    public Text initials;
+    public Text score;
+    public Text time;
+
+    public void UpdateScore(int index, HSPlayer player) {
+        position.text = index.ToString();
+        initials.text = player.initials;
+        score.text = player.playerScore.ToString() + "-" + player.computerScore.ToString();
+        time.text = player.timeScore.ToString("F1");
+    }
+}
