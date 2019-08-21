541052fd65f16f128d6c39653794c5ee47f8f706
Huang Junye <h.jun.ye@gmail.com>
2019-08-21 11:17:22 +0800

    Show credits at the same time as game over message

--- QPong-Unity-Version/Assets/Scripts/Game/GameHUD.cs
+++ QPong-Unity-Version/Assets/Scripts/Game/GameHUD.cs
@@ -9,9 +9,24 @@ public class GameHUD : MonoBehaviour
     public int yOffset = 120;
     public int centerOffset = 70;
     public int yOffsetWinMessage = 150;
+    public int yOffsetCreditString = 500;
 
     public string playerWinMessage = "You demonstrated quantum supremacy for the first time in human history!";
     public string computerWinMessage = "Classical computer still rules the world.";
+    public string creditString = @"This game was initiated in 
+IBM Qiskit Camp 2019 by 
+
+Huang Junye
+Jarrod Reilly
+Anastasia Jeffery
+James Weaver
+
+Arcade version was made by 
+
+Huang Junye
+Gregory Boland
+Ivan Duran
+";
     private bool showEndMessage = false;
     private string messageToShow;
 
@@ -20,17 +35,21 @@ public class GameHUD : MonoBehaviour
         GUI.skin = layout;
 
         var centeredLabelStyle = GUI.skin.GetStyle("Label");
-        var centeredButtonStyle = GUI.skin.GetStyle("Button");
+        var centeredCreditStyle = GUI.skin.GetStyle("TextField");
 
         centeredLabelStyle.alignment = TextAnchor.UpperCenter;
-        centeredButtonStyle.alignment = TextAnchor.MiddleCenter;
+        centeredCreditStyle.alignment = TextAnchor.UpperCenter;
 
+        // Show scores
         GUI.Label(new Rect(0 + xOffset, Screen.height/2 - centerOffset + yOffset, 100, 200), "" + PlayerScore1);
         GUI.Label(new Rect(0 + xOffset, Screen.height/2 - centerOffset - yOffset, 100, 200), "" + PlayerScore2);
 
+        // Show game over message and credits
         if (showEndMessage) {
             GUI.Label(new Rect(Screen.width / 2 - 1000, Screen.height/2 - 500 + yOffsetWinMessage, 2000, 1000),
                 messageToShow, centeredLabelStyle);
+            GUI.Label(new Rect(Screen.width / 2 - 1000, Screen.height/2 - 500 + yOffsetCreditString, 2000, 1200),
+                creditString, centeredCreditStyle);
         }
     }
 
