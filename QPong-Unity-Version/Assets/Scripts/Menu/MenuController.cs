2c799611d3d85dfe3d9bd945ef03924f2f2ca69c
boland <boland@us.ibm.com>
2019-09-09 07:07:41 -0400

    added the hold down of start to quit out of game

--- QPong-Unity-Version/Assets/Scripts/Menu/MenuController.cs
+++ QPong-Unity-Version/Assets/Scripts/Menu/MenuController.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class MenuController : MonoBehaviour
 {
     public int timeToExit = 10;
 
     bool startWasPressed = false;
+    float startButtonPressCount = 0f;
     private ArcadeAPIController arcadeAPIController;
 
     private void Awake() {
@@ -20,8 +22,32 @@ public class MenuController : MonoBehaviour
     void Update()
     {
         //Player needs to hit the Start button to start the game instead of any button
-        if (Input.GetButtonDown("Start")) {
-            GameController.Instance.StartGame();
+        if (Input.GetButtonDown("Start"))
+        {
+            //the game or maybe even close it out and go back to the app selection screen
+            startButtonPressCount = Time.time;
+            print("Start button press " + startButtonPressCount);
+
+        }
+        if (Input.GetButtonUp("Start"))
+        {
+
+            startButtonPressCount = Math.Abs(Time.time - startButtonPressCount);
+
+            print("time down " + startButtonPressCount);
+
+            if (startButtonPressCount >= 3.0f)
+            {
+                print("QUIT APP!!");
+                startButtonPressCount = 0f;
+
+                System.Diagnostics.Process.Start("osascript", "-e 'tell application \"AppMenu\" to activate'");
+                Application.Quit();
+            }else
+            {
+                GameController.Instance.StartGame();
+            }
+            startButtonPressCount = 0f;
         }
     }
 
